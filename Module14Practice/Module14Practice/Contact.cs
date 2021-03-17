24ccbade99df0ff4885a3162fc0733a003e10c3a
HelenArkh <alena.arkh@gmail.com>
2021-03-17 19:52:30 +0300

    Выполнение задания

---
+++ Module14Practice/Module14Practice/Contact.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Module14Practice
+{
+    public class Contact
+    {
+        public Contact(string name, int age, long phoneNumber, String email) // метод-конструктор
+        {
+            Name = name;
+            Age = age;
+            PhoneNumber = phoneNumber;
+            Email = email;           
+        }
+
+        public String Name { get; }
+        public int Age { get; }
+        public long PhoneNumber { get; }
+        public String Email { get; }
+    }
+}
---
+++ Module14Practice/Module14Practice/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Module14Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // создаём пустой список с типом данных Contact
+            var phoneBook = new List<Contact>();
+
+            // добавляем контакты
+            phoneBook.Add(new Contact("Игорь", 23, 79990000001, "igor@example.com"));
+            phoneBook.Add(new Contact("Сергей", 55, 79990000010, "serge@example.com"));
+            phoneBook.Add(new Contact("Анатолий", 49, 79990000011, "anatoly@example.com"));
+            phoneBook.Add(new Contact("Валерий", 21, 79990000012, "valera@example.com"));
+            phoneBook.Add(new Contact("Сергей", 20, 799900000013, "serg@example.com"));
+            phoneBook.Add(new Contact("Иннокентий", 39, 799900000013, "innokentii@example.com"));
+
+            // Сортировка по имени и возрасту
+            var sortedPhoneBook = phoneBook
+               .OrderBy(s => s.Name)
+               .ThenBy(s => s.Age);
+
+            foreach (var stud in sortedPhoneBook)
+                Console.WriteLine(stud.Name + ", " + stud.Age);
+        }
+    }
+}
